b3578f8c9ad0b1b702345bd7dd69bf380d3061c0
digexperience <jolly0903992@gmail.com>
2022-12-12 21:33:31 +0800

    final

--- clear.php
+++ clear.php
@@ -7,19 +7,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha512-P5MgMn1jBN01asBgU0z60Qk4QxiXo86+wlFahKrsQf37c9cro517WzVSPPV1tDKzhku2iJ2FVgL67wG03SGnNA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Learn IT Easy Online Shop | Shopping Cart</title>
 </head>
 <body>
     <div class="container">
         <div class="row mt-5">
             <div class="col-10">
-                <h1><i class="fa fa-store"></i>Easy Learn It Shop</h1>
+                <h1><i class="fa fa-store"></i> Learn IT Easy Online Shop</h1>
             </div>
             <div class="col-2 text-right">
                 <a href="cart.php" class="btn btn-primary">
                     <i class="fa fa-shopping-cart"></i> Cart
-                    <span class="badge badge-light">0</span>
+                    <span class="badge badge-light">
+                    <?php 
+                    unset($_SESSION['totalQuantity']);
+                    echo (isset($_SESSION['totalQuantity']) ? $_SESSION['totalQuantity'] : "0");
+                    ?>
+                    </span>
                 </a>
             </div>            
         </div>
--- confirm.php
+++ confirm.php
@@ -7,14 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha512-P5MgMn1jBN01asBgU0z60Qk4QxiXo86+wlFahKrsQf37c9cro517WzVSPPV1tDKzhku2iJ2FVgL67wG03SGnNA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Learn IT Easy Online Shop | Shopping Cart</title>
 </head>
 <body>
     <div class="container">
         <div class="row mt-5">
             <div class="col-10">
-                <h1><i class="fa fa-store"></i>Easy Learn It Shop</h1>
+                <h1><i class="fa fa-store"></i> Learn IT Easy Online Shop</h1>
             </div>
             <div class="col-2 text-right">
                 <a href="cart.php" class="btn btn-primary">
