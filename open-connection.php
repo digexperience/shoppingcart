b4a78554f1cf470607e6ee9ef4625be57329139a
digexperience <jolly0903992@gmail.com>
2022-12-12 20:25:47 +0800

    logout

--- open-connection.php
+++ open-connection.php
@@ -1,3 +1,6 @@
 <?php
-$con = msqli_connect("localhost","root","","shopcart")
+$con = msqli_connect("localhost","root","","shopcart");
+if ($con === false){
+    die("ERROR: Could not Connected". mysqli_connect_error());
+}
 ?>
